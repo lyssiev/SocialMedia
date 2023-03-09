6bdf4136ae0a53ee9f140c7dc66bef50facd6976
lyssiev <alyssa_venter@hotmail.co.uk>
2023-03-09 16:02:47 +0000

    Added exceptions files and interfaces given in project spec (changed location to socialmedia package)

--- src/socialmedia/Account.java
+++ src/socialmedia/Account.java
@@ -6,11 +6,34 @@ public class Account {
     private String handle;
     private String description;
 
+    public Account(String handle) {
+        this.id = ++latestId;
+        this.handle = handle;
+    }
     public Account(String handle, String description) {
         this.id = ++latestId;
         this.handle = handle;
         this.description = description;
     }
 
-    // getters and setters here
+    public int getId() {
+        return id;
+    }
+
+    public String getHandle() {
+        return handle;
+    }
+
+    public void setHandle(String handle) {
+        this.handle = handle;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
 }
--- src/socialmedia/MiniSocialMedia.java
+++ src/socialmedia/MiniSocialMedia.java
@@ -1,3 +1,26 @@
 package socialmedia;
+import java.io.IOException;
+import socialmedia.Account;
+import java.util.ArrayList;
 public class MiniSocialMedia {
+
+    ArrayList<Account> profiles = new ArrayList<Account>();
+
+    @Override
+    public int createAccount(String handle) throws IllegalHandleException, InvalidHandleException {
+
+        for (Account profile : profiles)
+        {
+            if (profile.getHandle().equals(handle)){
+                throw new IllegalHandleException("Handle already taken, please try again.");
+            }
+        }
+        if (handle.length() > 30 || handle.contains(" ") || handle == null)
+        {
+            throw new InvalidHandleException("Handle invalid. Please make sure it has less than 30 characters, no white spaces and is not empty.");
+        }
+        profiles.add(account);
+        return account.getId();
+    }
+
 }
